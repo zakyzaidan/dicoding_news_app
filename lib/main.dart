8bf942acb7df8d46395d7ce6ee5567b9626b79b4
Zaky Zaidan <zaky.zaidan624@gmail.com>
2024-01-01 16:03:17 +0700

    add repository

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:dicoding_news_app/article.dart';
+import 'package:dicoding_news_app/article_web_view.dart';
+import 'package:dicoding_news_app/detail_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => const NewsListPage(),
+        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+          article: ModalRoute.of(context)?.settings.arguments as Article,
+        ),
+        ArticleWebView.routeName:(context) => ArticleWebView(
+          url: ModalRoute.of(context)?.settings.arguments as String,
+        )
+      },
+    );
+  }
+}
+
+class NewsListPage extends StatelessWidget {
+  static const routeName = '/article_list';
+
+  const NewsListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("News App"),
+      ),
+      body: FutureBuilder<String>(
+        future: DefaultAssetBundle.of(context).loadString('assets/articles.json'),
+        builder: (context, snapshot) {
+          final List<Article> articles = parseArticles(snapshot.data);
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              return _buildArticleItem(context, articles[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildArticleItem(BuildContext context, Article article) {
+    return ListTile(
+      contentPadding:
+          const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      leading: Image.network(
+        article.urlToImage,
+        width: 100,
+        errorBuilder: (ctx, error, _) => const Center(child: Icon(Icons.error)),
+      ),
+      title: Text(article.title),
+      subtitle: Text(article.author),
+      onTap: (){
+        Navigator.pushNamed(context, ArticleDetailPage.routeName,
+        arguments: article);
+      },
+    );
+  }
+}
+
